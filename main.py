d2f12fa904355540d0385e7c521df954c9f9cc1f
Alexander <bmgula55@mail.ru>
2023-07-03 23:40:20 +0300

    refactor main.py add (res_amount, res_name)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 from datetime import datetime, date
 
-last_operations = 5
+count_last_operations = 5
 """
 Последние 5 выполненных (EXECUTED) операций выведены на экран.
 """
@@ -31,13 +31,14 @@ def open_json_data(path: str) -> list:
 
 
 def sort_date(value: list, last_operations: int):
+    """Сортирует по дате последние 'last_operations' - кол-во выполненных операций"""
     sorted_date = {}
     for i in value:
         try:
             if i["state"] == "EXECUTED":
                 sorted_date[i["date"]] = i
                 if len(sorted_date) == last_operations:
-                    return sorted(sorted_date, reverse=True)[:5], sorted_date
+                    return sorted(sorted_date, reverse=True)[:last_operations], sorted_date
         except:
             pass
 
@@ -47,9 +48,15 @@ def main(value):
     b = value[1]
 
     for i in a:
-        res_data = str(b[i]["date"]).split("T")[0].split("-")
-        res_description = b[i]["description"]
-        print(".".join(res_data[::-1]), res_description)
+        try:
+            res_data = str(b[i]["date"]).split("T")[0]
+            thedate = date.fromisoformat(res_data).strftime("%d.%m.%Y")
+        except:
+            pass
+        try:
+            res_description = b[i]["description"]
+        except:
+            pass
         try:
             res_from = b[i]["from"]
         except:
@@ -58,11 +65,22 @@ def main(value):
             res_to = b[i]["to"]
         except:
             res_to = None
+        try:
+            res_amount = b[i]["operationAmount"]["amount"]
+        except:
+            res_amount = None
+        try:
+            res_name = b[i]["operationAmount"]["currency"]["name"]
+        except:
+            res_name = None
+
+        print(thedate, res_description)
         print(res_from, "->", res_to)
+        print(res_amount, res_name)
         print("\n")
 
 
-result = open_json_data("operations.json")
-last_5_date = sort_date(result, last_operations)
+res_load_json = open_json_data("operations.json")
+last_count_date = sort_date(res_load_json, count_last_operations)
 
-main(last_5_date)
+main(last_count_date)
